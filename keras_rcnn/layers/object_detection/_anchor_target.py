ebd8a5291e3a2661966240f6095077749f4a39dc
Allen Goodman <allen.goodman@icloud.com>
2017-08-04 14:03:45 -0400

    refactored AnchorTarget

--- keras_rcnn/layers/object_detection/_anchor_target.py
+++ keras_rcnn/layers/object_detection/_anchor_target.py
@@ -1,11 +1,10 @@
 import keras.backend
 import keras.engine
-import tensorflow
 
 import keras_rcnn.backend
 
 
-class AnchorTarget(keras.engine.topology.Layer):
+class AnchorTarget(keras.layers.Layer):
     """Calculate proposal anchor targets and corresponding labels (label: 1 is positive, 0 is negative, -1 is do not care) for ground truth boxes
 
     # Arguments
@@ -21,11 +20,13 @@ class AnchorTarget(keras.engine.topology.Layer):
         (# of samples, ), (# of samples, 4)
     """
 
-    def __init__(self, allowed_border=0, clobber_positives=False, negative_overlap=0.3, positive_overlap=0.7, **kwargs):
+    def __init__(self, allowed_border=0, clobber_positives=False, negative_overlap=0.3, positive_overlap=0.7, stride=16, **kwargs):
         self.allowed_border = allowed_border
         self.clobber_positives = clobber_positives
         self.negative_overlap = negative_overlap
         self.positive_overlap = positive_overlap
+
+        self.stride = stride
 
         super(AnchorTarget, self).__init__(**kwargs)
 
@@ -33,16 +34,18 @@ class AnchorTarget(keras.engine.topology.Layer):
         super(AnchorTarget, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
-        gt_boxes, metadata = inputs
+        scores, gt_boxes, metadata = inputs
 
         # TODO: Fix usage of batch index
         shape = metadata[0, :2]
 
+        rr, cc = keras.backend.int_shape(scores)[1:-1]
+
         # 1. Generate proposals from bbox deltas and shifted anchors
-        anchors = keras_rcnn.backend.shift(shape, 16)
+        anchors = keras_rcnn.backend.shift((rr, cc), self.stride)
 
         # only keep anchors inside the image
-        indices, anchors = keras_rcnn.backend.inside_image(anchors, metadata, self.allowed_border)
+        indices, anchors = keras_rcnn.backend.inside_image(anchors, metadata[0], self.allowed_border)
 
         # 2. obtain indices of gt boxes with the greatest overlap, balanced labels
         argmax_overlaps_indices, labels = keras_rcnn.backend.label(anchors, gt_boxes, indices, self.negative_overlap, self.positive_overlap, self.clobber_positives)
@@ -54,10 +57,12 @@ class AnchorTarget(keras.engine.topology.Layer):
 
         # TODO: Why is bbox_reg_targets' shape (5, ?, 4)? Why is gt_boxes' shape (None, None, 4) and not (None, 4)?
         bbox_reg_targets = keras.backend.reshape(bbox_reg_targets, (-1, 4))
+
+        # TODO: implement inside and outside weights
         return [labels, bbox_reg_targets]
 
     def compute_output_shape(self, input_shape):
-        return [(None,), (None, 4)]
+        return [(None,), (None,)]
 
     def compute_mask(self, inputs, mask=None):
         # unfortunately this is required
