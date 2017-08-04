f14a02d026f182330f4c1f5833e49fb6cb02c0a0
jhung0 <jyhung@broadinstitute.org>
2017-08-04 15:24:39 -0400

    fix Regression loss layer

--- keras_rcnn/layers/losses.py
+++ keras_rcnn/layers/losses.py
@@ -13,7 +13,7 @@ class ClassificationLoss(keras.layers.Layer):
         rpn_classification = keras.backend.reshape(rpn_classification, [-1, 2])
         rpn_classification = keras.backend.gather(rpn_classification, keras_rcnn.backend.where(keras.backend.not_equal(rpn_labels, -1)))
         rpn_labels         = keras.backend.gather(rpn_labels, keras_rcnn.backend.where(keras.backend.not_equal(rpn_labels, -1)))
-        loss               = keras.backend.mean(keras.backend.categorical_crossentropy(rpn_labels, rpn_classification))
+        loss               = keras.backend.mean(keras.backend.sparse_categorical_crossentropy(rpn_labels, rpn_classification))
 
         return loss
 
@@ -34,14 +34,19 @@ class RegressionLoss(keras.layers.Layer):
 
         super(RegressionLoss, self).__init__(**kwargs)
 
-    def _loss(self, y_true, y_pred):
+    def _loss(self, rpn_bbox_targets, rpn_regression, rpn_labels):
         # Robust L1 Loss
-        x = y_true[:, :, :, 4 * self.anchors:] - y_pred
+        rpn_regression = keras.backend.reshape(rpn_regression, [-1, 4])
+        rpn_regression = keras.backend.gather(rpn_regression, keras_rcnn.backend.where(keras.backend.not_equal(rpn_labels, -1)))
+        rpn_labels     = keras.backend.gather(rpn_labels, keras_rcnn.backend.where(keras.backend.not_equal(rpn_labels, -1)))
+
+        x = rpn_bbox_targets - rpn_regression
 
         mask = keras.backend.less_equal(keras.backend.abs(x), 1.0)
         mask = keras.backend.cast(mask, keras.backend.floatx())
 
-        a_x = y_true[:, :, :, :4 * self.anchors]
+        a_x = keras_rcnn.backend.where(keras.backend.not_equal(rpn_labels, 0))
+        a_x = keras.backend.cast(a_x, keras.backend.floatx())
 
         a_y = mask * (0.5 * x * x) + (1 - mask) * (keras.backend.abs(x) - 0.5)
 
@@ -52,13 +57,15 @@ class RegressionLoss(keras.layers.Layer):
         b = keras.backend.epsilon() + a_x
         b = keras.backend.sum(b)
 
-        return 1.0 * (a / b)
+        loss = 1.0 * (a / b)
+
+        return loss
 
     def call(self, inputs):
-        y_true, y_pred = inputs
+        y_true, y_pred, labels = inputs
 
-        loss = self._loss(y_true, y_pred)
+        loss = self._loss(y_true, y_pred, labels)
 
         self.add_loss(loss, inputs=inputs)
 
-        return y_pred
+        return loss
